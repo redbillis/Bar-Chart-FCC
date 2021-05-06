409ae8ce8b416744d7941733ad7cbeeeeba25bde
Red <redbillis@protonmail.com>
2021-05-06 11:58:52 +0300

    Adding some style and making progress with the project.

--- index.js
+++ index.js
@@ -20,6 +20,73 @@ let drawCanvas = () => {
   svg.attr('height', height);
 }
 
-let generate = () => {
-  
+let generateScales = () => {
+  // Creating a Scale for Bar height.
+  heightScale = d3.scaleLinear()
+                  .domain([0, d3.max(values, (item) => {
+                    return item[1];
+                  })])
+                  .range([0, height - 2 * padding]);
+
+  // Creating a Scale for placing Bars horizontally.
+  xScale = d3.scaleLinear()
+                  .domain([0, values.length - 1])
+                  .range([padding, width - padding]);
+
+  // Converting the Strings that have the Dates into Numerical Values.
+  let datesArray = values.map((item) => {
+    return new Date(item[0]);
+  })
+
+  // Creating a Scale for the x-axis of Dates.
+  xAxisScale = d3.scaleTime()
+                  .domain([d3.min(datesArray), d3.max(datesArray)])
+                  .range([padding, width - padding]);
+
+  // Creating a Scale for the y-axis of GDP.
+  yAxisScale = d3.scaleLinear()
+                  .domain([0, d3.max(values, (item) => {
+                    return item[1];
+                  })])
+                  .range([height - padding, padding]);
 }
+
+let drawBars = () => {
+  // My chart should have a rect element for each data point with a corresponding class="bar" displaying the data.
+  svg.selectAll('rect')
+      .data(values)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('width', (width - 2 * padding) / values.length);
+}
+
+let generateAxis = () => {
+  // Both axes should contain multiple tick labels, each with a corresponding class="tick". D3 Bottom and Left take care of that!
+  let xAxis = d3.axisBottom(xAxisScale);
+  let yAxis = d3.axisLeft(yAxisScale);
+
+  // My chart should have a g element x-axis with a corresponding id="x-axis".
+  svg.append('g')
+      .call(xAxis)
+      .attr('id', 'x-axis')
+      .attr('transform', 'translate(0, ' + (height - padding) + ')');
+
+  // My chart should have a g element y-axis with a corresponding id="y-axis".
+  svg.append('g')
+      .call(yAxis)
+      .attr('id', 'y-axis')
+      .attr('transform', 'translate(' + (padding) + ', 0)');
+}
+
+req.open('GET', url, true);
+req.onload = () => {
+  data = JSON.parse(req.responseText);
+  values = data.data;
+  console.log(values);
+  drawCanvas();
+  generateScales();
+  drawBars();
+  generateAxis();
+}
+req.send();
